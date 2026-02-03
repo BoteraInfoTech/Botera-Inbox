7e64479d10a176ad270e1634387e3d8baf93bcc1
Akil Saiyad <akil.saiyad@group.one>
2026-02-03 12:23:26 +0530

    rename routes

--- src/Services/Inbox-API/router/facebook.js
+++ src/Services/Inbox-API/router/facebook.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 // open Endpoints
 
-router.get('/facebook', (req, res) => {
+router.get('/', (req, res) => {
   return res
     .status(200)
     .json({ message: 'webhook is healthy', code: 200, status: 'success' });
 });
-router.post('/facebook', FacebookReceiver);
+router.post('/', FacebookReceiver);
 
 export default router;
